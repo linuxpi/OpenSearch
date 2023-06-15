d148dc64a4af7f307452fc8df0169e4c3141d804
Varun Bansal <bansvaru@amazon.com>
2023-06-15 22:40:48 +0530

    [Remote Store] Add latest local and remote refresh timestamps in the stats api response (#7922)
    * Add latest local and remote refresh timestamps in the stats api response
    
    Signed-off-by: bansvaru <bansvaru@amazon.com>
    
    * add a separate field for clock time to allow backpressure to use nano precision fields
    
    Signed-off-by: bansvaru <bansvaru@amazon.com>
    
    * fix spotloss failures
    
    Signed-off-by: bansvaru <bansvaru@amazon.com>
    
    * correct update validation for remote refresh clock time
    
    Signed-off-by: bansvaru <bansvaru@amazon.com>
    
    * remove unused wall clock time lag
    
    Signed-off-by: bansvaru <bansvaru@amazon.com>
    
    * revert unintended changes to backtracker precision time fields
    
    Signed-off-by: bansvaru <bansvaru@amazon.com>
    
    * fix indentation
    
    Signed-off-by: bansvaru <bansvaru@amazon.com>
    
    ---------
    
    Signed-off-by: bansvaru <bansvaru@amazon.com>
    (cherry picked from commit de51a496a2e727d74aef76d7fb2714b95a582169)

--- server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
+++ server/src/main/java/org/opensearch/index/shard/RemoteStoreRefreshListener.java
@@ -180,7 +180,8 @@ public final class RemoteStoreRefreshListener implements ReferenceManager.Refres
             return;
         }
         beforeSegmentsSync(isRetry);
-        long refreshTimeMs = segmentTracker.getLocalRefreshTimeMs(), refreshSeqNo = segmentTracker.getLocalRefreshSeqNo();
+        long refreshTimeMs = segmentTracker.getLocalRefreshTimeMs(), refreshClockTimeMs = segmentTracker.getLocalRefreshClockTimeMs();
+        long refreshSeqNo = segmentTracker.getLocalRefreshSeqNo();
         long bytesBeforeUpload = segmentTracker.getUploadBytesSucceeded(), startTimeInNS = System.nanoTime();
         boolean shouldRetry = true;
         try {
@@ -232,9 +233,9 @@ public final class RemoteStoreRefreshListener implements ReferenceManager.Refres
                             // Start metadata file upload
                             uploadMetadata(localSegmentsPostRefresh, segmentInfos);
                             clearStaleFilesFromLocalSegmentChecksumMap(localSegmentsPostRefresh);
-                            onSuccessfulSegmentsSync(refreshTimeMs, refreshSeqNo);
                             ((InternalEngine) indexShard.getEngine()).translogManager().setMinSeqNoToKeep(lastRefreshedCheckpoint + 1);
                             checkpointPublisher.publish(indexShard, checkpoint);
+                            onSuccessfulSegmentsSync(refreshTimeMs, refreshClockTimeMs, refreshSeqNo, lastRefreshedCheckpoint, checkpoint);
                             // At this point since we have uploaded new segments, segment infos and segment metadata file,
                             // along with marking minSeqNoToKeep, upload has succeeded completely.
                             shouldRetry = false;
@@ -278,11 +279,17 @@ public final class RemoteStoreRefreshListener implements ReferenceManager.Refres
         segmentTracker.incrementTotalUploadsStarted();
     }
 
-    private void onSuccessfulSegmentsSync(long refreshTimeMs, long refreshSeqNo) {
+    private void onSuccessfulSegmentsSync(
+        long refreshTimeMs,
+        long refreshClockTimeMs,
+        long refreshSeqNo,
+        long lastRefreshedCheckpoint,
+        ReplicationCheckpoint checkpoint
+    ) {
         // Update latest uploaded segment files name in segment tracker
         segmentTracker.setLatestUploadedFiles(latestFileNameSizeOnLocalMap.keySet());
         // Update the remote refresh time and refresh seq no
-        updateRemoteRefreshTimeAndSeqNo(refreshTimeMs, refreshSeqNo);
+        updateRemoteRefreshTimeAndSeqNo(refreshTimeMs, refreshClockTimeMs, refreshSeqNo);
         // Reset the backoffDelayIterator for the future failures
         resetBackOffDelayIterator();
         // Cancel the scheduled cancellable retry if possible and set it to null
@@ -378,6 +385,7 @@ public final class RemoteStoreRefreshListener implements ReferenceManager.Refres
      * Updates the last refresh time and refresh seq no which is seen by local store.
      */
     private void updateLocalRefreshTimeAndSeqNo() {
+        segmentTracker.updateLocalRefreshClockTimeMs(System.currentTimeMillis());
         segmentTracker.updateLocalRefreshTimeMs(System.nanoTime() / 1_000_000L);
         segmentTracker.updateLocalRefreshSeqNo(segmentTracker.getLocalRefreshSeqNo() + 1);
     }
@@ -385,7 +393,8 @@ public final class RemoteStoreRefreshListener implements ReferenceManager.Refres
     /**
      * Updates the last refresh time and refresh seq no which is seen by remote store.
      */
-    private void updateRemoteRefreshTimeAndSeqNo(long refreshTimeMs, long refreshSeqNo) {
+    private void updateRemoteRefreshTimeAndSeqNo(long refreshTimeMs, long refreshClockTimeMs, long refreshSeqNo) {
+        segmentTracker.updateRemoteRefreshClockTimeMs(refreshClockTimeMs);
         segmentTracker.updateRemoteRefreshTimeMs(refreshTimeMs);
         segmentTracker.updateRemoteRefreshSeqNo(refreshSeqNo);
     }
