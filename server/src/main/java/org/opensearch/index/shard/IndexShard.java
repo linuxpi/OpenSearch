50f5425f4ebdd65a14e473e91849dbc3c149157d
bansvaru <bansvaru@amazon.com>
2023-08-09 11:02:49 +0530

    Final Refactor
    Signed-off-by: bansvaru <bansvaru@amazon.com>

--- server/src/main/java/org/opensearch/index/shard/IndexShard.java
+++ server/src/main/java/org/opensearch/index/shard/IndexShard.java
@@ -2256,7 +2256,6 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
         // acquiring a snapshot from the translog causes a sync which causes the global checkpoint to be pulled in,
         // and an engine can be forced to close in ctor which also causes the global checkpoint to be pulled in.
         final String translogUUID = store.readLastCommittedSegmentsInfo().getUserData().get(TRANSLOG_UUID_KEY);
-        if (translogUUID == null) return;
         final long globalCheckpoint = Translog.readGlobalCheckpoint(translogConfig.getTranslogPath(), translogUUID);
         replicationTracker.updateGlobalCheckpointOnReplica(globalCheckpoint, "read from translog checkpoint");
     }
@@ -4692,7 +4691,6 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
             .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
         store.incRef();
         remoteStore.incRef();
-        String segmentNFile = null;
         try {
             final Directory storeDirectory;
             if (recoveryState.getStage() == RecoveryState.Stage.INDEX) {
@@ -4708,7 +4706,7 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
             } else {
                 storeDirectory = store.directory();
             }
-            segmentNFile = copySegmentFiles(storeDirectory, remoteDirectory, null, uploadedSegments, overrideLocal);
+            copySegmentFiles(storeDirectory, remoteDirectory, null, uploadedSegments, overrideLocal);
 
             if (remoteSegmentMetadata != null) {
                 final SegmentInfos infosSnapshot = store.buildSegmentInfos(
@@ -4733,7 +4731,6 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
             store.decRef();
             remoteStore.decRef();
         }
-        return segmentNFile;
     }
 
     /**
@@ -4815,7 +4812,6 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
                 }
             }
             for (String file : uploadedSegments.keySet()) {
-                logger.info(String.format("segment files downloaded from remote %s", file));
                 long checksum = Long.parseLong(uploadedSegments.get(file).getChecksum());
                 if (overrideLocal || localDirectoryContains(storeDirectory, file, checksum) == false) {
                     storeDirectory.copyFrom(sourceRemoteDirectory, file, file, IOContext.DEFAULT);
